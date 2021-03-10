7d02e53e4f2afb1e648f53258cd227bf0d8c7365
Sirius <83930223@qq.com>
2021-03-10 16:12:42 +0800

    deploy

---
+++ precache-manifest.15d18338871f6b4cde2037195067a0df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a53a846e2d7b0e6644c1",
+    "url": "/siricash-vue-website/css/app.bcf30d03.css"
+  },
+  {
+    "revision": "b37d9f085c9c7846e5742e964ed4f44d",
+    "url": "/siricash-vue-website/index.html"
+  },
+  {
+    "revision": "a53a846e2d7b0e6644c1",
+    "url": "/siricash-vue-website/js/app.5e453c9f.js"
+  },
+  {
+    "revision": "57c2033d28d6d4acffa4",
+    "url": "/siricash-vue-website/js/chunk-vendors.0f16db56.js"
+  },
+  {
+    "revision": "0e8705c436f627d21ee2d9572eca9163",
+    "url": "/siricash-vue-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/siricash-vue-website/robots.txt"
+  }
+]);
